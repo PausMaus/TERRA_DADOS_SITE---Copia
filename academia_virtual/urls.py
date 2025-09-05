dde888108af8b46bbf6c0348293908f13a2dc897
PausMaus <viniciusamaral250@gmail.com>
2025-09-05 02:38:35 -0300

    UX e Academia Virtual
    - melhorias basicas no UX
    - Melhor coleta de dados
    - atualizações AV

--- academia_virtual/urls.py
+++ academia_virtual/urls.py
@@ -7,4 +7,5 @@ app_name = 'academia_virtual'
 urlpatterns = [
     path("", views.index, name="index"),
     path("item/<int:item_id>/", views.detalhe, name="detalhe"),
+    path("exercicio/<int:exercicio_id>/", views.exercicio_detalhe, name="exercicio_detalhe"),
 ]
